c1c114208c300bf4675c39185d0743e343cfd3cf
gabriel <gabriel_hc@hotmail.fr>
2025-04-08 10:08:42 +0400

    update ingestion.py : create directories outside the ingestion function

--- ingestion.py
+++ ingestion.py
@@ -3,28 +3,14 @@ import subprocess
 import shutil
 from glob import glob
 from zipfile import ZipFile
-
-
-def create_dir(dir_path):
-    """
-    Creates a directory if it doesn't already exist.
-    """
-    try:
-        os.mkdir(dir_path)
-        print(f"Directory '{dir_path}' created successfully.")
-    except FileExistsError:
-        print(f"Directory '{dir_path}' already exists.")
-    except PermissionError:
-        print(f"Permission denied: Unable to create '{dir_path}'.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+from utils import create_dir
 
 
 def ingestion_pipeline():
     """
     Pipeline to download, unzip, organize, and prepare data for further processing.
     """
-    print("Downloading the data...")
+    print("Ingesting the data...")
     try:
         subprocess.run(
             [
@@ -51,10 +37,6 @@ def ingestion_pipeline():
         print(f"Error unzipping data: {e}")
         return
 
-    print("Creating the directories for the data...")
-    create_dir("./data")
-    create_dir("./data/raw")
-
     print("Moving the data we are interested in to the './data/raw/' directory...")
     source = "./predict-energy-behavior-of-prosumers"
     destination = "./data/raw"
@@ -70,6 +52,8 @@ def ingestion_pipeline():
 
     # TODO: upload the data on S3.
 
+    print("Ingestion pipeline completed successfully.")
+
 
 if __name__ == "__main__":
     try:
@@ -77,8 +61,12 @@ if __name__ == "__main__":
             print("Data already saved in ./data/raw/")
         else:
             ingestion_pipeline()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print("Running the ingestion pipeline...")
+    except FileNotFoundError:
+        print("Directory './data/raw/' does not exist. Creating it now...")
+        create_dir("./data")
+        create_dir("./data/raw")
         ingestion_pipeline()
-        print("Ingestion pipeline completed.")
+    except PermissionError:
+        print("Permission denied: Unable to access './data/raw/'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
